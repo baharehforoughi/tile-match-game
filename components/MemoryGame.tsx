8d6f6a993e13ef90e832489cf140228a938b99ac
baharehforoughi <bhr.foroughi@gmail.com>
2024-03-18 18:09:22 +0100

    create MemoryGame logic

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -1,26 +1,90 @@
+"use client";
+import Image from "next/image";
+
 import React from "react";
-import "app/globals.css";
+import { useEffect } from "react";
+import Stopwatch from "./Stopwatch";
 
-const MemoryGame = () => {
+const generateDeck = () => {
+  const memoryCards = [
+    "aquamarine",
+    "lavenderpink",
+    "mintgreen",
+    "paleyellow",
+    "peach",
+    "periwinkle",
+    "salmonpink",
+    "skyblue",
+  ];
+  const deck = [...memoryCards, ...memoryCards];
+  return deck.sort(() => Math.random() - 0.5);
+};
+export default function MemoryGame() {
+  const [cards, setCards] = React.useState<string[]>(generateDeck());
+  const [flipped, setFlipped] = React.useState<number[]>([]);
+  const [solved, setSolved] = React.useState<number[]>([]);
+
+  useEffect(() => {
+    const checkForMatch = () => {
+      const [first, second] = flipped;
+      if (cards[first] === cards[second]) {
+        setSolved([...solved, ...flipped]);
+      }
+      setFlipped([]);
+    };
+
+    if (flipped.length === 2) {
+      setTimeout(() => checkForMatch(), 1000);
+    }
+  }, [cards, flipped, solved]);
+
+  const handleClick = (index: number) => {
+    if (!flipped.includes(index) && flipped.length < 2) {
+      setFlipped([...flipped, index]);
+    }
+  };
+
+  const gameOver = solved.length === cards.length;
+  const resetGame = () => {
+    setCards(generateDeck());
+    setFlipped([]);
+    setSolved([]);
+  };
+
   return (
-    <div className="game-container">
-      <header className="game-header"></header>
-
-      {/* TODO - Correct logic for tile flips */}
-      <div className="game-board">
-        {Array.from({ length: 16 }).map((_, index) => (
+    <div className="text-center ">
+      <h1>
+        Memory Game
+        {/* <Stopwatch /> */}
+      </h1>
+      {gameOver && <h2 className="p-5">You WON!</h2>}
+      <div className="grid grid-cols-4 gap-5 mt-5 bg-gray-300 p-5">
+        {cards.map((card, index) => (
           <div
-            key={index}
-            className={`tile ${
-              index === 0 || index === 5 || index === 10 || index === 15
-                ? "flipped"
+            className={`flex justify-center items-center text-4xl font-bold text-black bg-white w-28 h-28 transform cursor-pointer transition-transform duration-300 ${
+              flipped.includes(index) || solved.includes(index)
+                ? "rotate-180"
                 : ""
             }`}
-          />
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {flipped.includes(index) || solved.includes(index) ? (
+              <Image
+                className="rotate-180"
+                src={`/memory_card/${card}.png`}
+                fill
+                alt="Memory Card"
+              />
+            ) : (
+              " "
+            )}
+          </div>
         ))}
       </div>
+      <button onClick={resetGame} className="p-5 bg-slate-500 rounded-md mt-5">
+        Restart
+      </button>
     </div>
   );
-};
-
-export default MemoryGame;
+}
