47d1bfbbede34e6c819497db009409ae8ca39803
baharehforoughi <bhr.foroughi@gmail.com>
2024-04-22 22:08:53 +0200

    remove intermediate page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,102 +1,64 @@
-//index.tsx
-"use client";
+//game.tsx
+import React, { useState, useCallback } from "react";
+import MemoryGame from "@/components/MemoryGame";
+import Scoreboard from "@/components/Scoreboard";
+import GameWinPopup from "@/components/GameWinPopup";
 
-import React from "react";
-import { useRouter } from "next/navigation";
-import BaharehLinkedInQR from "/public/LinkedInScanable/BaharehLinkedIn.png";
-import LeeLinkedInQR from "/public/LinkedInScanable/LeeLinkedIn.png";
-import Image from "next/image";
+const GamePage = () => {
+  const [isGameActive, setIsGameActive] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+  const [scores, setScores] = useState<Array<{ name: string; score: number }>>(
+    []
+  );
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [startTime, setStartTime] = useState<Date | null>(null);
 
-const IndexPage = () => {
-  const router = useRouter();
+  const handleGameWin = useCallback(() => {
+    const endTime = new Date();
+    if (startTime) {
+      const durationInSeconds =
+        (endTime.getTime() - startTime.getTime()) / 1000;
+      const pointsDeductedPerSecond = 5;
+      const calculatedScore = Math.max(
+        0,
+        Math.round(10000 - durationInSeconds * pointsDeductedPerSecond)
+      );
 
-  // Function to handle button click and navigate to the game page 'Start New Game'
-  const startGame = () => {
-    router.push("/game"); // Use router.push to navigate to the game page from welcome page
-  };
+      setIsGameActive(false);
+      setScore(calculatedScore);
+      setShowPopup(true);
+    }
+  }, [startTime]);
+
+  const handleNameSubmit = useCallback((name: string, score: number) => {
+    const newScoreEntry = { name, score };
+    setScores((prevScores) =>
+      [...prevScores, newScoreEntry].sort((a, b) => b.score - a.score)
+    );
+    setShowPopup(false); // hides the popup
+  }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center px-4">
-      <div className="bg-white shadow-2xl rounded-2xl p-10 m-4 w-full max-w-4xl space-y-6">
-        <h1 className="text-5xl font-bold text-center text-gray-800">
-          Welcome to Tile Match Game
-        </h1>
-        <div className="bg-indigo-100 p-6 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-2">
-            About this game:
-          </h2>
-          <p className="text-md text-gray-700">
-            Welcome to our game app, a product of our intensive Full-Stack
-            Javascript and Python Academy Bootcamp journey. With a passion for
-            innovation and a dedication to mastering the latest technologies,
-            weve created this app as a showcase of our skills and expertise.
-          </p>
-        </div>
-        <div className="bg-indigo-100 p-6 rounded-lg shadow-lg">
-          <p className="text-md text-gray-700">
-            Harnessing the power of Javascript, Next.js, Tailwind, HTML & CSS,
-            Typescript, Python, and various database technologies such as
-            PostgreSQL and SQLAlchemy, weve crafted an immersive gaming
-            experience. Our journey through GraphQL, Docker, RESTAPI, and
-            Postico has equipped us with the tools to deliver seamless
-            performance and functionality.
-          </p>
-        </div>
-        <div className="bg-indigo-100 p-6 rounded-lg shadow-lg">
-          <h2 className="font-bold text-md text-gray-700">
-            Meet the developers:
-          </h2>
-          <br></br>
-          <div className="flex justify-between items-center space-x-4">
-            <div className="flex-1">
-              <h3>Bahareh Foroughi</h3>
-              <a
-                href="https://www.linkedin.com/in/bhrforoughi/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src={BaharehLinkedInQR.src}
-                  alt="Bahareh's LinkedIn QR Code"
-                  width={125}
-                  height={75}
-                />
-              </a>
-            </div>
-
-            <div className="flex-1">
-              <h3>Lee-Anne van der Merwe</h3>
-              <a
-                href="https://www.linkedin.com/in/lee-anne-van-der-merwe/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src={LeeLinkedInQR.src}
-                  alt="Lee-Anne's LinkedIn QR Code"
-                  width={125}
-                  height={75}
-                />
-              </a>
-            </div>
+    <div className="flex justify-center items-center h-screen bg-gray-100 p-4 user-select-none select-none">
+      <div className="flex justify-between container mx-auto p-4 bg-white rounded-lg shadow-md">
+        <div className="flex flex-col items-start space-y-4">
+          <div className="container">
+            <MemoryGame onGameWin={handleGameWin} setStartTime={setStartTime} />
           </div>
-          <p className="text-md text-gray-700 mt-4">
-            From conception to execution, every aspect of this app represents
-            our commitment to excellence and our drive to push the boundaries of
-            whats possible in the world of game development. Join us on this
-            adventure as we continue to explore new horizons and redefine the
-            gaming landscape.
-          </p>
         </div>
-        <button
-          onClick={startGame}
-          className="w-full py-3 bg-blue-600 text-white font-bold rounded-lg hover:bg-blue-800 transition duration-300"
-        >
-          Start New Game
-        </button>
+        <div className="flex flex-col items-end space-y-4">
+          {/* <Scoreboard scores={scores} setStartTime={setStartTime} /> */}
+          <Scoreboard setStartTime={setStartTime} />
+          {showPopup && (
+            <GameWinPopup
+              score={score}
+              onNameSubmit={handleNameSubmit}
+              onHide={() => setShowPopup(false)} // Direct control for hiding
+            />
+          )}
+        </div>
       </div>
     </div>
   );
 };
-
-export default IndexPage;
+export default GamePage;
